459a05391213c306fc8c78ade78cb6545e82e833
zakirIndia <48102616+zakirIndia@users.noreply.github.com>
2019-07-29 23:19:26 +0530

    Update desrouleaux.py

--- picoCTF-2018/Forensics/Desrouleaux /desrouleaux.py
+++ picoCTF-2018/Forensics/Desrouleaux /desrouleaux.py
@@ -5,19 +5,21 @@ import socket
 
 def first(sock):
     res = sock.recv(250).decode('utf-8')
-    sock.send('248.0.40.3\n'.encode('utf-8'))
+    sock.send('246.69.53.233\n'.encode('utf-8'))  # Your Answer For Non Changing Question 1 
     res = sock.recv(250).decode('utf-8')
     res = res.split('\n')
     return res
 
 
 def which(ip):
-    if ip == "159.244.90.48":
+    if ip == "215.239.98.18":  #IP 1
+        return "1\n"
+    if ip == "246.69.53.233":  #IP 2
         return "3\n"
-    if ip == "248.0.40.3":
-        return "4\n"
-    if ip == "109.165.249.213":
-        return "2\n"
+    if ip == "231.205.245.44": #IP 3
+        return "1\n"
+    if ip == "251.165.34.242": #IP 3
+        return "3\n"
 
 
 def second(sock, first_res):
@@ -41,12 +43,14 @@ def third(sock, ans):
 if __name__ == '__main__':
 
 
-    port = 14079
-    host = "2018shell1.picoctf.com"
+    port = 54782
+    host = "2018shell.picoctf.com"
 
     # Loop over third question
-    # for third_ans in [float(j)/100 for j in range(110, 1501, 1)] :
-    for third_ans in [1.28, 1.29] :
+    i = 1.00
+    j = 2.00
+    for third_ans in [float(j)/100 for j in range(110, 1501, 1)] :
+      for third_ans in [i, j] :
 
         print("Trying", third_ans)
 
@@ -69,3 +73,6 @@ if __name__ == '__main__':
         sock.close()
 
         time.sleep(1.0)
+
+        i = i + 0.01
+        j = j + 0.01
